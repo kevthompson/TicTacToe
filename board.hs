4f7d6f2eaf362d6d204454e3c6f907305a641cbb
Kevin <kevthompson97@gmail.com>
2018-07-22 14:42:07 -0500

    printing an array and taking input, woo

--- board.hs
+++ board.hs
@@ -1,6 +1,7 @@
 -- don't have array of the board 
 -- instead, keep an ordered list of the moves so far and reconstruct board from that
 -- to check for wins, check all of following if %2 of each index is the same.
+    -- The above strategy is probably terrible
 -- | (1, 4, 7), (2, 5, 8), (3, 6, 9), 
 -- | (1, 2, 3), (4, 5, 6), (7, 8, 9), 
 -- | (1, 5, 9), (3, 5, 7)
@@ -13,13 +14,22 @@ import Data.List
 --     | otherwise     = getMove $ (read input :: Int):n
 --     where input <- getLine
 
-let n = []
+newBoard = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+
+getPiece :: Char -> Char
+getPiece a
+    | a == '1' = 'X'
+    | a == '2' = 'O'
+    | otherwise = ' '
+
+-- printBoard a = intersperse ' ' $ intersperse '|' $ map (getPiece) $ concat $ intercalate "\n" $ map(map(show)) a
+stringify :: [[Int]] -> [Char]
+stringify board = intercalate "\n" $ map(show) board ++ ["\n"]
+    
 main = do
     input <- getLine
-    if length n > 9 
-        then error "list got too long"
-        else if length n == 9 
-            then print n
-        else do 
-            let n = (read input :: Int) ++ n
-            main
+    let n = read input :: Int
+    putStr $ stringify newBoard
+    -- if n > 9 
+    --     then print "value too large"
+
